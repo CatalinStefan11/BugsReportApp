ae9929fc94aeb0f6024e10100ea3588bfc2ca1dc
Catalin <catalin.stefann11@gmail.com>
2019-12-10 00:23:12 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
+
+import { AuthService } from './user/auth.service';
+
+import { MessageService } from './messages/message.service';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  //animations: [slideInAnimation]
+})
+export class AppComponent {
+  pageTitle = 'Bugs Report Application';
+  loading = true;
+
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+
+  get userName(): string {
+    if (this.authService.currentUser) {
+      return this.authService.currentUser.userName;
+    }
+    return '';
+  }
+
+  constructor(private authService: AuthService,
+              private router: Router,
+              private messageService: MessageService) {
+    router.events.subscribe((routerEvent: Event) => {
+      this.checkRouterEvent(routerEvent);
+    });
+  }
+
+  checkRouterEvent(routerEvent: Event): void {
+    if (routerEvent instanceof NavigationStart) {
+      this.loading = true;
+    }
+
+    if (routerEvent instanceof NavigationEnd ||
+        routerEvent instanceof NavigationCancel ||
+        routerEvent instanceof NavigationError) {
+      this.loading = false;
+    }
+  }
+
+
+  logOut(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/welcome');
+  }
+}
